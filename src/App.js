63796370519c258cd9dd7bf55912d1b9560c72e7
ntzwilly <ntzwilly@gmail.com>
2022-04-19 14:16:29 +0200

    Configure routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+// import Error from "./pages/Error";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      Hello from App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="rooms" element={<Rooms />} />
+        <Route path="/rooms/:slug" element={<SingleRoom />} />
+      </Routes>
     </div>
   );
 }
