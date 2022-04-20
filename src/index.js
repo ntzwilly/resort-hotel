6a49dedcb0430fe78f085c791e5dfead54a91601
ntzwilly <ntzwilly@gmail.com>
2022-04-20 13:47:10 +0200

    Setup Context API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { RoomProvider } from "./Context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>
 );
-
