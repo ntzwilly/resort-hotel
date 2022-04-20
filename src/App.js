4fbedb04ff39fd8284d999307ff3693625c30fb1
ntzwilly <ntzwilly@gmail.com>
2022-04-20 10:44:00 +0200

    Create the navbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import "./App.css";
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
-// import Error from "./pages/Error";
+import Error from "./pages/Error";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="rooms" element={<Rooms />} />
         <Route path="/rooms/:slug" element={<SingleRoom />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
   );
