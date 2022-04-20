868ce5369d6877e0664dd13735288188861e1712
ntzwilly <ntzwilly@gmail.com>
2022-04-20 12:06:28 +0200

    Create Title component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,23 @@ import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
+import Services from "../components/Services";
 
 export default function Home() {
   return (
-    <Hero>
-      <Banner
-        title="luxurious rooms"
-        subtitle="deluxe rooms starting at $299"
-      >
-        <Link to='/rooms' className="btn-primary">
-          our rooms
-        </Link>
+    <>
+      <Hero>
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
         </Banner>
-    </Hero>
+      </Hero>
+      <Services />
+    </>
   );
 }
 
